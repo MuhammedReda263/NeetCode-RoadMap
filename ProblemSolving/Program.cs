568eabc91f1b39bc1d549670c782361bbf2a87ba
Muhammed Reda <moreda263@gmail.com>
2025-07-16 06:42:13 +0300

    First 4 problems on Arrays&Hashing

---
+++ ProblemSolving/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ProblemSolving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = {1,6,1,5};
+            //Console.WriteLine(Solution.hasDuplicate(arr)); 
+            //int[] arr2 = Solution.RunningSum(arr);
+            //foreach (int i in arr2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //string s1 = "ahmed";
+            //string s2 = "amedh";
+            //Console.WriteLine(Solution.IsAnagram(s1,s2));
+            int[] nums = [4,5,6]; int target = 10;
+
+            int [] arr =Solution.TwoSum(nums, target);
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+}
---
+++ ProblemSolving/Solution.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    public class Solution
+    {
+
+       
+       public static bool hasDuplicate(int[] nums)
+        {
+            HashSet<int> numbers = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                numbers.Add(nums[i]);
+            }
+            if (numbers.Count == nums.Length)
+                return false;
+            else
+                return true;
+        }
+
+        public static int[] RunningSum(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            result[0] = nums[0];
+            for (int i = 1; i < nums.Length;i++)
+            {
+                result[i] = result[i - 1] + nums[i];
+            }
+            return result;
+
+        }
+
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s == null || t == null) return false;
+            if (s.Length != t.Length) return false;
+            var s1 = s.ToCharArray(); 
+            var t1 = t.ToCharArray(); 
+            Array.Sort(s1);
+             Array.Sort(t1);
+
+            return s1.SequenceEqual(t1);
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length;i++)
+            {
+  
+                for (int j = i+1 ; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }   
+                }
+                
+               
+            }
+          return new int[0];
+        }
+    }
+}
